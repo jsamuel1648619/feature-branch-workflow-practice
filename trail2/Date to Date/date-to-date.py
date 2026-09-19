76e10256ed43668b5274e3a12e4a759aaba004bb
jsamuel <j.samuel1648619@gmail.com>
2026-09-19 17:35:53 +0900

    [Passed] Date to Date (48ms, 16MB)

--- trail2/Date to Date/date-to-date.py
+++ trail2/Date to Date/date-to-date.py
@@ -4,22 +4,16 @@ m1, d1, m2, d2 = map(int, input().split())
 # m2월 d2일까지 몇일? b
 # m1월 d1일까지 몇일? a
 # ans = b-a
-b = d2
-for i in range(1,m2):
-    if i in [1,3,5,7,8,10,12]:
-        b+= 31
-    elif i==2:
-        b+= 28
-    else:
-        b+=30
-a = d1
-for i in range(1,m1):
-    if i in [1,3,5,7,8,10,12]:
-        a+= 31
-    elif i==2:
-        a+= 28
-    else:
-        a+=30
+def mon_day(m,d):
+    total = d
+    for i in range(1,m):
+        if i in [1,3,5,7,8,10,12]:
+            total+= 31
+        elif i==2:
+            total+= 28
+        else:
+            total+=30 
+    return total
 
-ans = b-a+1
+ans = mon_day(m2,d2)-mon_day(m1,d1)+1
 print(ans)
