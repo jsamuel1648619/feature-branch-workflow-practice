4fdee2b7c02ae63d84ae1ca3f5822122d717b5e2
jsamuel <j.samuel1648619@gmail.com>
2026-09-21 08:32:23 +0900

    [Passed] 사각형의 총 넓이 2 (58ms, 17MB)

--- trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,37 +1,25 @@
-OFFSET = 100
-MAX_R = 200
-
-# 변수 선언 및 입력
 n = int(input())
-rects = [
-    tuple(map(int, input().split()))
-    for _ in range(n)
-]
+lst = [map(int,input().split())for _ in range(n)]
+offset = 100
+m = 2*offset + 1
 
-checked = [
-    [0] * (MAX_R + 1)
-    for _ in range(MAX_R + 1)
-]
+arr = [[0]*m for _ in range(m)]
 
-for x1, y1, x2, y2 in rects:
-    # OFFSET을 더해줍니다.
-    x1, y1 = x1 + OFFSET, y1 + OFFSET
-    x2, y2 = x2 + OFFSET, y2 + OFFSET
+for x1,y1,x2,y2 in lst:
+    x1 += offset
+    x2 += offset
+    y1 += offset
+    y2 += offset
+    for i in range(x1,x2):
+        for j in range(y1,y2):
+            arr[i][j] = 1
     
-    # 직사각형을 칠해줍니다.
-    # 격자 단위로 진행하는 문제이므로
-    # x2, y2에 등호가 들어가지 않음에 유의합니다.
-    for x in range(x1, x2):
-        for y in range(y1, y2):
-            checked[x][y] = 1
+ans=0
+for i in range(m):
+    for j in range(m):
+        if arr[i][j]==1:
+            ans+=1
 
-# 직사각형들이 덮인 영역의 넓이를 구합니다.
-area = 0
-for x in range(0, MAX_R + 1):
-    for y in range(0, MAX_R + 1):
-        if checked[x][y]:
-            area += 1
+print(ans)
 
-print(area)
-
-##
+    
