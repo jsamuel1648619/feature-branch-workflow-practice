fd102b12b2d15a8b75f3b49cef5e263c95b0b9b0
jsamuel <j.samuel1648619@gmail.com>
2026-09-21 22:27:56 +0900

    [Passed] 연속되는 수 2 (71ms, 16MB)

---
+++ trail2/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+max_cnt = 1
+for i in range(n):
+    cnt = 1
+    for j in range(i+1,n):
+        if arr[i]==arr[j]:
+            cnt+=1
+            max_cnt = max(max_cnt,cnt)
+        else:
+            break
+
+print(max_cnt)
