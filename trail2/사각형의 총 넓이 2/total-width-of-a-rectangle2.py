a78ea5adeab28f6f7d1be9d78dbcc6589cc612ea
jsamuel <j.samuel1648619@gmail.com>
2026-09-21 22:37:18 +0900

    [Passed] 사각형의 총 넓이 2 (63ms, 17MB)

--- trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,25 +1,24 @@
-n = int(input())
-lst = [map(int,input().split())for _ in range(n)]
 offset = 100
-m = 2*offset + 1
+m = 2*offset +1
 
 arr = [[0]*m for _ in range(m)]
 
-for x1,y1,x2,y2 in lst:
-    x1 += offset
-    x2 += offset
-    y1 += offset
-    y2 += offset
+n = int(input())
+for _ in range(n):
+    x1,y1,x2,y2 = map(int,input().split())
+
+    x1+=offset
+    y1+=offset
+    x2+=offset
+    y2+=offset
     for i in range(x1,x2):
         for j in range(y1,y2):
-            arr[i][j] = 1
-    
-ans=0
+            arr[i][j] =1
+
+cnt = 0 
 for i in range(m):
     for j in range(m):
-        if arr[i][j]==1:
-            ans+=1
+        if arr[i][j] ==1:
+            cnt+=1
 
-print(ans)
-
-    
+print(cnt) 
