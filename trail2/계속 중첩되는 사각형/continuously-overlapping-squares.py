bff0298d18e2cceb2656a67a05f4ca33231dc0c0
jsamuel <j.samuel1648619@gmail.com>
2026-09-21 21:57:58 +0900

    [Passed] 계속 중첩되는 사각형 (56ms, 17MB)

---
+++ trail2/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -0,0 +1,26 @@
+offset = 100
+m = 2*offset + 1
+
+arr = [[0]*m for _ in range(m)]
+
+n = int(input())
+for k in range(n):
+    x1,y1,x2,y2 = map(int,input().split())
+
+    x1+=offset
+    y1+=offset
+    x2+=offset
+    y2+=offset
+
+    for i in range(x1,x2):
+        for j in range(y1,y2):
+            arr[i][j]=k%2
+    
+
+cnt=0
+for i in range(m):
+    for j in range(m):
+        if arr[i][j]==1:
+            cnt +=1
+
+print(cnt)
